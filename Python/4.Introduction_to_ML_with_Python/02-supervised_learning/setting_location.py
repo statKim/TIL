99bba4781a448a2670b71175366fdbe871dba371
statKim <hyunsung1021@gmail.com>
2019-01-31 22:18:33 +0900

    Supervised learning

---
+++ Python/4.Introduction_to_ML_with_Python/02-supervised_learning/setting_location.py
@@ -0,0 +1,6 @@
+# 모듈이 다른 경로에 있기 때문에 모듈이 있는 경로에 추가해줌(한 번만 해놓으면 됨!!!)
+import sys
+sys.path.insert(0, "C:\\Users\\hyunsungKim\\ITStudy\\TIL\\Python\\4.Introduction_to_ML_with_Python")
+# sys.path
+# 혹시 여러번 실행했을 때 추가된 경로 삭제하는 방법
+# sys.path.remove("C:\\Users\\hyunsungKim\\ITStudy\\TIL\\Python\\4.Introduction_to_ML_with_Python")
