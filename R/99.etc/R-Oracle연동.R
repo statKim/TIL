da8d7f0cce81e2e477e2fb2c1c99da15a196bad5
statKim <hyunsung1021@gmail.com>
2019-02-23 23:18:16 +0900

    R-Oracle 연동

--- R/99.etc/R-Oracle연동.R
+++ R/99.etc/R-Oracle연동.R
@@ -2,26 +2,29 @@
 ##### R & Oracle 연동
 ######################
 
+# 설치시 순서가 중요! => 순서 틀릴 시 에러
 install.packages("rJava")
 install.packages("DBI")
 install.packages("RJDBC")
 
-#제일 먼저 로딩
+# 순서대로 import
 library(rJava)
-#Sys.setenv(JAVA_HOME='C:\\Program Files\\Java\\jre1.8.0_161')
 library(DBI)
-#rJava에 의존적이기 때문에, rJava 먼저 로딩해야 한다.
-library(RJDBC) 
+library(RJDBC) # rJava에 의존적
 
 
-##Oracle 연동(Oracle 11g)
-#driver
-drv <- JDBC("oracle.jdbc.driver.OracleDriver","C:\\oraclexe\\app\\oracle\\product\\11.2.0\\server\\jdbc\\lib\\ojdbc6.jar")
+## Oracle DB 연동(Oracle 11g)
+# driver 객체 생성
+drv <- JDBC("oracle.jdbc.driver.OracleDriver", "C:\\oraclexe\\app\\oracle\\product\\11.2.0\\server\\jdbc\\lib\\ojdbc6.jar")
 
-##DB 연동
-#connection 객체 생성
-conn <- dbConnect(drv, "jdbc:oracle:thin:@//(아이피):(포트번호)/(SID)", "(접속 아이디)", "(접속 비밀번호)")
-#쿼리 입력
-rst <- dbGetQuery(conn, "SELECT * FROM board")
-#출력
-rst
+# connection 객체 생성(DB connect)
+# conn <- dbConnect(drv, "jdbc:oracle:thin:@//(아이피):(포트번호)/(SID)", "(접속 아이디)", "(접속 비밀번호)")
+conn <- dbConnect(drv, "jdbc:oracle:thin:@//127.0.0.1:1521/XE", "SCOTT", "TIGER")
+
+# SQL query 날려서 결과 가져오기
+query <- "SELECT * FROMM EMP"	# 여기서는 ";"를 하면 안됨!
+result <- dbGetQuery(conn, query)
+result
+
+# DB disconnect
+dbDisconnect(conn)
